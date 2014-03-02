acdab104437a6630a11718b3f2fa840194252d25
Adam Schwartz <aschwartz@hubspot.com>
2014-03-01 22:32:30 -0500

    Compiled

--- unpacked-extension/js/app.js
+++ unpacked-extension/js/app.js
@@ -1,27 +1,26 @@
 (function() {
-  var App, Section, SectionModel, Sections, SectionsCollection, app, _sections,
-    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
-    __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-
+  var App, Section, SectionModel, Sections, SectionsCollection, app, _sections;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  };
   window.log = function() {
     if (this.console && this.console.log) {
       return console.log.apply(console, Array.prototype.slice.call(arguments));
     }
   };
-
-  App = (function(_super) {
+  App = (function() {
     var onDrag;
-
-    __extends(App, _super);
-
+    __extends(App, Backbone.View);
     function App() {
       this.onDrop = __bind(this.onDrop, this);
-      return App.__super__.constructor.apply(this, arguments);
+      App.__super__.constructor.apply(this, arguments);
     }
-
     App.prototype.el = 'body';
-
     App.prototype.events = {
       'click .add-section': 'addSection',
       'drop': 'onDrop',
@@ -30,33 +29,29 @@
       'dragenter': 'onDrag',
       'dragover': 'onDrag'
     };
-
     App.prototype.initialize = function() {
       var sections;
       return sections = new Sections({
         collection: this.collection
       }).render();
     };
-
     App.prototype.onDrop = function(e) {
-      var file, files, onload, reader, _i, _len, _ref, _ref1;
+      var file, files, onload, reader, _i, _len, _ref, _ref2;
       e = e.originalEvent;
-      if ((e != null ? (_ref = e.dataTransfer) != null ? (_ref1 = _ref.files) != null ? _ref1.length : void 0 : void 0 : void 0) > 0) {
-        onload = (function(_this) {
-          return function(e) {
-            var data, _ref2, _ref3;
-            if (file.type === 'text/csv') {
-              data = d3.csv.parseRows(atob(e.target.result.slice(21)));
-              _this.makeTable(data);
-            } else if (((_ref2 = file.type) != null ? (_ref3 = _ref2.split('\/')) != null ? _ref3[0].toLowerCase() : void 0 : void 0) === 'image') {
-              _this.collection.add({
-                type: 'image',
-                value: e.target.result
-              });
-            }
-            return _this.$el.removeClass('dragenter dragover');
-          };
-        })(this);
+      if ((e != null ? (_ref = e.dataTransfer) != null ? (_ref2 = _ref.files) != null ? _ref2.length : void 0 : void 0 : void 0) > 0) {
+        onload = __bind(function(e) {
+          var data, _ref3, _ref4;
+          if (file.type === 'text/csv') {
+            data = d3.csv.parseRows(atob(e.target.result.slice(21)));
+            this.makeTable(data);
+          } else if (((_ref3 = file.type) != null ? (_ref4 = _ref3.split('\/')) != null ? _ref4[0].toLowerCase() : void 0 : void 0) === 'image') {
+            this.collection.add({
+              type: 'image',
+              value: e.target.result
+            });
+          }
+          return this.$el.removeClass('dragenter dragover');
+        }, this);
         files = e.dataTransfer.files;
         for (_i = 0, _len = files.length; _i < _len; _i++) {
           file = files[_i];
@@ -68,11 +63,9 @@
       e.preventDefault();
       return false;
     };
-
     App.prototype.onMouseUp = function(e) {
       return this.$el.removeClass('dragenter dragover');
     };
-
     onDrag = function(e) {
       e = e.originalEvent;
       $('body').addClass('dragenter');
@@ -80,20 +73,17 @@
       e.preventDefault();
       return false;
     };
-
     document.body.ondragover = function(e) {
       $('body').addClass('dragover');
       e.dataTransfer.dropEffect = 'move';
       e.preventDefault();
       return false;
     };
-
     App.prototype.addSection = function() {
       return this.collection.add({
         value: '<p>New paragraph...</p>'
       });
     };
-
     App.prototype.makeGraph = function(columns, data) {
       var height, line, margin, svg, width, x, xAxis, y, yAxis, yDomain;
       margin = {
@@ -129,9 +119,8 @@
       svg.append("path").datum(data).attr("class", "line").attr("d", line);
       return "<svg width=" + (width + margin.left + margin.right) + " height=" + (height + margin.top + margin.bottom) + ">" + (svg.html()) + "</svg>";
     };
-
     App.prototype.makeTable = function(data) {
-      var column, columns, graphHtml, html, row, rows, _i, _j, _k, _len, _len1, _len2;
+      var column, columns, graphHtml, html, row, rows, _i, _j, _k, _len, _len2, _len3;
       columns = data.shift();
       rows = data;
       html = "<table><thead><tr>";
@@ -140,10 +129,10 @@
         html += "<th>" + column + "</th>";
       }
       html += '</tr></thead><tbody>';
-      for (_j = 0, _len1 = rows.length; _j < _len1; _j++) {
+      for (_j = 0, _len2 = rows.length; _j < _len2; _j++) {
         row = rows[_j];
         html += "<tr>";
-        for (_k = 0, _len2 = row.length; _k < _len2; _k++) {
+        for (_k = 0, _len3 = row.length; _k < _len3; _k++) {
           column = row[_k];
           html += "<td>" + column + "</td>";
         }
@@ -156,71 +145,51 @@
         value: "<div class='chart'>" + (html + graphHtml) + "</div>"
       });
     };
-
     return App;
-
-  })(Backbone.View);
-
-  SectionModel = (function(_super) {
-    __extends(SectionModel, _super);
-
+  })();
+  SectionModel = (function() {
+    __extends(SectionModel, Backbone.Model);
     function SectionModel() {
-      return SectionModel.__super__.constructor.apply(this, arguments);
+      SectionModel.__super__.constructor.apply(this, arguments);
     }
-
     SectionModel.prototype.defaults = {
       type: 'text',
       caption: '',
       value: ''
     };
-
     return SectionModel;
-
-  })(Backbone.Model);
-
-  SectionsCollection = (function(_super) {
-    __extends(SectionsCollection, _super);
-
+  })();
+  SectionsCollection = (function() {
+    __extends(SectionsCollection, Backbone.Collection);
     function SectionsCollection() {
-      return SectionsCollection.__super__.constructor.apply(this, arguments);
+      SectionsCollection.__super__.constructor.apply(this, arguments);
     }
-
     SectionsCollection.prototype.model = SectionModel;
-
     return SectionsCollection;
-
-  })(Backbone.Collection);
-
-  Section = (function(_super) {
-    __extends(Section, _super);
-
+  })();
+  Section = (function() {
+    __extends(Section, Backbone.View);
     function Section() {
-      return Section.__super__.constructor.apply(this, arguments);
+      Section.__super__.constructor.apply(this, arguments);
     }
-
     Section.prototype.className = 'section';
-
     Section.prototype.events = {
       'focus .section-content, .section-caption': 'addFocus',
       'blur .section-content, .section-caption': 'removeFocus',
       'click .section-delete': 'handleDelete',
       'input': 'input'
     };
-
     Section.prototype.input = function(e) {
       this.model.set('value', this.$el.html());
     };
-
     Section.prototype.addFocus = function() {
       $('body').addClass('section-focused');
       return this.$el.addClass('section-focused');
     };
-
     Section.prototype.removeFocus = function() {
       $('body').removeClass('section-focused');
       return this.$el.removeClass('section-focused');
     };
-
     Section.prototype.render = function() {
       var caption, type, value, _ref;
       _ref = this.model.toJSON(), value = _ref.value, type = _ref.type, caption = _ref.caption;
@@ -230,33 +199,25 @@
       this.$el.attr('data-type', type).html("<div class=\"section-helpers\">\n    <div class=\"section-drag-handle\"></div>\n    <div class=\"section-delete\"></div>\n</div>\n<div class=\"section-content\">\n    " + value + "\n</div>\n<div class=\"section-caption\">\n    " + caption + "\n</div>");
       return this;
     };
-
     Section.prototype.handleDelete = function(e) {
       this.model.destroy();
       this.remove();
       return e.preventDefault();
     };
-
     return Section;
-
-  })(Backbone.View);
-
-  Sections = (function(_super) {
-    __extends(Sections, _super);
-
+  })();
+  Sections = (function() {
+    __extends(Sections, Backbone.View);
     function Sections() {
-      return Sections.__super__.constructor.apply(this, arguments);
+      Sections.__super__.constructor.apply(this, arguments);
     }
-
     Sections.prototype.el = '.sections';
-
     Sections.prototype.initialize = function() {
       return this.listenTo(this.collection, 'add', function(section) {
         this.addChild(section);
         return this.postRender();
       });
     };
-
     Sections.prototype.addChild = function(section) {
       var sectionView;
       sectionView = new Section({
@@ -264,17 +225,13 @@
       });
       return this.$el.append(sectionView.render().el);
     };
-
     Sections.prototype.render = function() {
-      this.collection.each((function(_this) {
-        return function(section) {
-          return _this.addChild(section);
-        };
-      })(this));
+      this.collection.each(__bind(function(section) {
+        return this.addChild(section);
+      }, this));
       this.postRender();
       return this;
     };
-
     Sections.prototype.postRender = function() {
       var mediumOptions;
       mediumOptions = {
@@ -298,11 +255,8 @@
       });
       return this;
     };
-
     return Sections;
-
-  })(Backbone.View);
-
+  })();
   _sections = [
     {
       type: 'header',
@@ -328,9 +282,7 @@
       value: '<p>\nConsuming a horizontal length of only 14 letterspaces, each sparkline\nin the big table above provides a look at the price and the changes in\nprice for every day for years, and the overall time pattern. <i>This financial\ntable reports 24 numbers accurate to 5 significant digits; the accompanying\nsparklines show about 14,000 numbers readable from 1 to 2 significant digits.\nThe idea is to be approximately right rather than exactly wrong.</i>&nbsp;<font size="-1">1</font>\n</p>\n\n<p>By showing recent change in relation to many past changes, sparklines\nprovide a context for nuanced analysis—and, one hopes, better decisions.\nMoreover, the year-long daily history reduces <i>recency bias,</i> the persistent\nand widespread over-weighting of recent events in making decisions.\nTables sometimes reinforce recency bias by showing only current levels\nor recent changes; sparklines improve the attention span of tables.\n</p>\n\n<p>Tables of numbers attain maximum densities of only 300 characters per\nsquare inch or 50 characters per square centimeter. In contrast, graphical\ndisplays have far greater resolutions; a cartographer notes "the resolving\npower of the eye enables it to differentiate to 0.1 mm where provoked to\ndo so."&nbsp;<font size="-1">2</font> &nbsp;Distinctions at 0.1 mm mean 250 per linear inch, which implies\n60,000 per square inch or 10,000 per square centimeter, which is plenty.</p>'
     }
   ];
-
   app = {};
-
   app.init = function() {
     var appView;
     $('body').removeClass('dragenter dragover');
@@ -341,7 +293,6 @@
       collection: new SectionsCollection(_sections)
     });
   };
-
   app.saveExport = function() {
     document.body.classList.add('capturing');
     return setTimeout(function() {
@@ -353,9 +304,7 @@
       }, 0);
     }, 300);
   };
-
   $(function() {
     return app.init();
   });
-
 }).call(this);
